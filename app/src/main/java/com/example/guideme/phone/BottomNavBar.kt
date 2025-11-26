399abe590248798c4055abde846fa0f41b7d8348
tinakralimarkova1 <67720045+tinakralimarkova1@users.noreply.github.com>
2025-11-26 14:27:19 +0800

    Fixed dialpad ui

--- app/src/main/java/com/example/guideme/phone/BottomNavBar.kt
+++ app/src/main/java/com/example/guideme/phone/BottomNavBar.kt
@@ -1,6 +1,11 @@
 package com.example.guideme.phone
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Contacts
 import androidx.compose.material.icons.filled.Schedule
@@ -11,53 +16,74 @@ import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.guideme.tts.TTS
 
+
 @Composable
 fun BottomNavBar(navController: NavController, currentRoute: String) {
-    NavigationBar(tonalElevation = 8.dp,
+    Box(
         modifier = Modifier
-            .padding(bottom = 140.dp, top = 5.dp))
+            .fillMaxSize()
+            .heightIn(min= 80.dp, max =90.dp)
+            //.padding(top=10.dp)// ⬅️ increase from 56dp → 64/72/80dp as needed
+    ) {
+
+        NavigationBar(
+            tonalElevation = 8.dp,
+            modifier = Modifier
+                .padding(bottom = 5.dp, top = 5.dp)
+                .clip(RoundedCornerShape(16.dp))
+            //.heightIn(min = 50.dp, max = 80.dp)
+
+        )
 
 
-    {
+        {
+            Row(modifier = Modifier.padding(top=14.dp)
 
-        NavigationBarItem(
-            selected = currentRoute == "favorites",
-            onClick = {
-                TTS.speak("You are entering favorites.")
-                navController.navigate("favorites") {
-                    popUpTo("favorites") { inclusive = false }
-                }
-            },
-            icon = { Icon(Icons.Filled.Star, contentDescription = "Favorites") },
-            label = { Text("Favorites", fontSize = 17.sp) }
-        )
-        NavigationBarItem(
-            selected = currentRoute == "recents",
-            onClick = {
-                TTS.speak("You are entering recents.")
-                navController.navigate("recents") {
-                    popUpTo("recents") { inclusive = false }
-                }
-            },
-            icon = { Icon(Icons.Filled.Schedule, contentDescription = "Recents") },
-            label = { Text("Recents", fontSize = 17.sp) }
-        )
-        NavigationBarItem(
-            selected = currentRoute == "contacts",
-            onClick = {
-                TTS.speak("You are entering contacts.")
-                navController.navigate("contacts") {
-                    popUpTo("contacts") { inclusive = false }
-                }
-            },
-            icon = { Icon(Icons.Filled.Contacts, contentDescription = "Contacts") },
-            label = { Text("Contacts", fontSize = 17.sp) }
-        )
+            ) {
+
+
+                NavigationBarItem(
+
+                    selected = currentRoute == "favorites",
+                    onClick = {
+                        TTS.speak("You are entering favorites.")
+                        navController.navigate("favorites") {
+                            popUpTo("favorites") { inclusive = false }
+                        }
+                    },
+                    icon = { Icon(Icons.Filled.Star, contentDescription = "Favorites") },
+                    label = { Text("Favorites", fontSize = 17.sp) }
+                )
+                NavigationBarItem(
+                    selected = currentRoute == "recents",
+                    onClick = {
+                        TTS.speak("You are entering recents.")
+                        navController.navigate("recents") {
+                            popUpTo("recents") { inclusive = false }
+                        }
+                    },
+                    icon = { Icon(Icons.Filled.Schedule, contentDescription = "Recents") },
+                    label = { Text("Recents", fontSize = 17.sp) }
+                )
+                NavigationBarItem(
+                    selected = currentRoute == "contacts",
+                    onClick = {
+                        TTS.speak("You are entering contacts.")
+                        navController.navigate("contacts") {
+                            popUpTo("contacts") { inclusive = false }
+                        }
+                    },
+                    icon = { Icon(Icons.Filled.Contacts, contentDescription = "Contacts") },
+                    label = { Text("Contacts", fontSize = 17.sp) }
+                )
+            }
+        }
     }
 
 }
